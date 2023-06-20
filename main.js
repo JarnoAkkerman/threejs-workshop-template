d1470c4193c508c037bbfca6a1962537d62e8e1c
JarnoAkkerman <61318202+JarnoAkkerman@users.noreply.github.com>
2023-06-20 20:19:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { ARButton } from "three/examples/jsm/webxr/ARButton";
+
+function main() {
+  // Let's get started...
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    // Animation stuff goes here...
+  }
+
+  animate();
+}
+
+main();
